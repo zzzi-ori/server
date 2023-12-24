6b02e0e8ec7b195d0832caacb2d7412b7106ac15
Lee-DoHa <70997596+Lee-DoHa@users.noreply.github.com>
2023-12-24 23:32:05 +0900

    init setting

--- zzio/src/main/java/com/game/zzio/domain/Rank.java
+++ zzio/src/main/java/com/game/zzio/domain/Rank.java
@@ -1,4 +1,13 @@
 package com.game.zzio.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Rank {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
